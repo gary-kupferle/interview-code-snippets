f9aef919cfa1de37ddc369a7cf06169f03ca260a
Gary Kupferle <gary.kupferle+github@gmail.com>
2020-08-28 08:40:17 -0500

    additional examples from https://github.com/RefactoringGuru/refactoring-examples/tree/master/simple/java

---
+++ conditional.java
@@ -0,0 +1,14 @@
+double disabilityAmount() {
+    if (seniority < 2) {
+      return 0;
+    }
+    if (monthsDisabled > 12) {
+      return 0;
+    }
+    if (isPartTime) {
+      return 0;
+    }
+    // Compute the disability amount.
+    // ... more code
+    // returns disability amount
+}
